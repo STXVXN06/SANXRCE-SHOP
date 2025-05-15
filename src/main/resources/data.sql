15feba5efc376a2a149bc737ae1038b3930fea67
Sarce22 <sebastianarce482@gmail.com>
2025-05-15 17:40:08 -0500

    deleteSqlfile

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Rol (id_Rol, name_rol) VALUES (1, 'Admin');
-INSERT INTO Rol (id_Rol, name_rol) VALUES (2, 'Cliente');
-INSERT INTO Rol (id_Rol, name_rol) VALUES (3, 'Empleado');
---  Add any other roles you need
